8b4930654c07295ce7b5a7a361db027a1a27ae8a
Raye <Raye@RAYEPC>
2017-02-17 11:42:20 +0900

    Center 계산하지 않던 문제 수정

--- Universe2D/Sprite.cpp
+++ Universe2D/Sprite.cpp
@@ -7,67 +7,72 @@
 #include "Texture.h"
 
 Sprite::Sprite(std::wstring fileName)
-    : m_ImageWidth(.0f), m_ImageHeight(.0f),
-    m_ColorA(0), m_ColorR(0), m_ColorG(0), m_ColorB(0),
-    m_Renderer(nullptr), m_Texture(nullptr), IsLoaded(false)
+	: m_ImageWidth(.0f), m_ImageHeight(.0f),
+	m_ColorA(0), m_ColorR(0), m_ColorG(0), m_ColorB(0),
+	m_Renderer(nullptr), m_Texture(nullptr), IsLoaded(false)
 {
-    m_Renderer = Application::GetInstance()->GetRenderer();
-    m_Texture = ResourceManager::GetInstance()->LoadTextureFromFile(fileName);
+	m_Renderer = Application::GetInstance()->GetRenderer();
+	m_Texture = ResourceManager::GetInstance()->LoadTextureFromFile(fileName);
 
-    // 텍스쳐가 정상적으로 불러와졌는지 검사
-    IsLoaded = m_Texture ? true : false;
+	// 텍스쳐가 정상적으로 불러와졌는지 검사
+	IsLoaded = m_Texture ? true : false;
 
-    // 텍스쳐가 정상적이지않으므로 그냥 중지
-    // 오류 처리에 따라서 오류 메세지가 담긴 이미지로 바꾸는 등 후처리를 할 수 있을 듯.
-    if (!IsLoaded)
-        return;
+	// 텍스쳐가 정상적이지않으므로 그냥 중지
+	// 오류 처리에 따라서 오류 메세지가 담긴 이미지로 바꾸는 등 후처리를 할 수 있을 듯.
+	if (!IsLoaded)
+		return;
 
-    m_ImageWidth = m_Texture->GetWidth();
-    m_ImageHeight = m_Texture->GetHeight();
+	m_ImageWidth = m_Texture->GetWidth();
+	m_ImageHeight = m_Texture->GetHeight();
 
-	m_Position = m_Center;
+	m_Center = {
+		m_ImageWidth / 2,
+		m_ImageHeight / 2
+	};
 
-    D3DXCreateSprite(m_Renderer->GetDevice(), &m_D3DSprite);
+	//m_Position = m_Center;
+
+	D3DXCreateSprite(m_Renderer->GetDevice(), &m_D3DSprite);
 }
 Sprite* Sprite::Create(std::wstring fileName)
 {
-    auto instance = new Sprite(fileName);
-    return instance;
+	auto instance = new Sprite(fileName);
+	return instance;
 }
 Sprite::~Sprite()
 {
-    SAFE_RELEASE(m_D3DSprite);
+	SAFE_RELEASE(m_D3DSprite);
 
-    m_Texture = nullptr;
-    m_Renderer = nullptr;
+	m_Texture = nullptr;
+	m_Renderer = nullptr;
 }
 
 void Sprite::Resize(float width, float height)
 {
-    m_Scale = {
-        width / m_ImageWidth,
-        height / m_ImageHeight
-    };
+	m_Scale = {
+		width / m_ImageWidth,
+		height / m_ImageHeight
+	};
 
 	m_Center = m_Scale / 2;
 }
 
 void Sprite::Render()
 {
-    if (!m_Visible || !IsLoaded) return;
+	if (!m_Visible || !IsLoaded) return;
 
-    Object::Render();
+	Object::Render();
 
-    // 하나의 이미지에서 그려질 부분의 영역(Rect)을 설정 할 수 있음
-    // 추후에 파라미터로 제공할 예정
-    RECT srcRect;
-    SetRect(&srcRect, 0, 0, m_ImageWidth, m_ImageHeight);
+	// 하나의 이미지에서 그려질 부분의 영역(Rect)을 설정 할 수 있음
+	// 추후에 파라미터로 제공할 예정
+	RECT srcRect;
+	SetRect(&srcRect, 0, 0, m_ImageWidth, m_ImageHeight);
 
-    m_D3DSprite->Begin(D3DXSPRITE_ALPHABLEND);
-    m_D3DSprite->SetTransform(&m_Matrix);
-    m_D3DSprite->Draw(
-        m_Texture->GetD3DTexture(),
-        &srcRect, NULL, &D3DXVECTOR3(-m_ImageWidth / 2, -m_ImageHeight / 2, .0f), //-m_ImageWidth / 2, -m_ImageHeight / 2
-        D3DCOLOR_ARGB(255 - m_ColorA, 255 - m_ColorR, 255 - m_ColorG, 255 - m_ColorB));
-    m_D3DSprite->End();
+	m_D3DSprite->Begin(D3DXSPRITE_ALPHABLEND);
+	m_D3DSprite->SetTransform(&m_Matrix);
+	m_D3DSprite->Draw(
+		m_Texture->GetD3DTexture(),
+		&srcRect, NULL, &D3DXVECTOR3(-m_ImageWidth / 2, -m_ImageHeight / 2, .0f), //-m_ImageWidth / 2, -m_ImageHeight / 2
+		D3DCOLOR_ARGB(255 - m_ColorA, 255 - m_ColorR, 255 - m_ColorG, 255 - m_ColorB));
+	m_D3DSprite->End();
 }
